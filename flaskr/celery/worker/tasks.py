865e25b16c7318177e467d1feb1112e07dfb99a1
liuhaifeng <liuhaifeng@sensetime.com>
2025-03-31 15:36:57 +0800

    uograde(other):

--- flaskr/celery/worker/tasks.py
+++ flaskr/celery/worker/tasks.py
@@ -1,6 +1,17 @@
 from flaskr.celery.celery import app
 
 
-@app.task
-def add(x, y):
+@app.task(
+    # 如果为true，任务的第一个参数将是任务实例本身
+    bind=True,
+    queue='my_queue',
+    ignore_result=False,
+    # 任务将在执行完成后才发送确认消息
+    acks_late=True,
+    # 设置在特定异常时重试任务
+    # autoretry_for=(Exception,),
+    # 引入抖动，避免重试任务集中执行
+    retry_jitter=True
+)
+def add(self, x, y):
     return x + y
